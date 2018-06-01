c492f45c3f31f7ed5ddf4ba103dfa91747588a78
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-06-01 17:54:36 -0400

    server running

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+
 const env = 'development';
 const config = require('./knexfile.js')[env];
 const knex = require('knex')(config);
-const sql = knex('').toString();
+const sql = knex('herSoiree').toString();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const PORT = process.envPORT || 3016;
 console.log(sql);
 knex.destroy()
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.get('/', (request, response)=> {
+  response.send(`herSoiree: Listening on port no.${PORT}`)
+});
+
+
+app.listen(PORT, ()=>{
+  console.log(`herSoiree: Listening on port no.${PORT}`)
+});
